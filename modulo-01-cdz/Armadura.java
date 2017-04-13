53d6bfcbe023c29adae3cb87327de0312ee7300f
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2017-04-13 17:52:51 -0300

    feat(Saint): adicionar categoria e gênero aos saints e suas armaduras.

--- modulo-01-cdz/Armadura.java
+++ modulo-01-cdz/Armadura.java
@@ -1,8 +1,10 @@
 public class Armadura {
     private String constelacao;
+    private Categoria categoria;
     
-    public Armadura(String constelacao) {
+    public Armadura(String constelacao, Categoria categoria) {
         this.constelacao = constelacao;
+        this.categoria = categoria;
     }
     
 }
--- modulo-01-cdz/Saint.java
+++ modulo-01-cdz/Saint.java
@@ -2,6 +2,7 @@ public class Saint {
     private String nome;
     private Armadura armadura;
     private boolean armaduraVestida;
+    private Genero genero = Genero.NAO_INFORMADO;
     
     public Saint(String nome, Armadura armadura) {
         this.nome = nome;
@@ -12,4 +13,26 @@ public class Saint {
         this.armaduraVestida = true;
     }
     
+    // camelCase
+    public boolean getArmaduraVestida() {
+        return this.armaduraVestida;
+    }
+    
+    public Genero getGenero() {
+        return this.genero;
+    }
+    
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
 }
+
+
+
+
+
+
+
+
+
+
