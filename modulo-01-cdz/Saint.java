2e99dd744f2672d0fb5ce8caa03cb2cba886bb03
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2017-04-17 16:31:45 -0300

    feat(Saint): adicionar restante das constelações válidas para saints de ouro.

--- modulo-01-cdz/Saint.java
+++ modulo-01-cdz/Saint.java
@@ -19,11 +19,18 @@ public class Saint {
         } else if (this.armadura.getCategoria() == Categoria.OURO) {
             this.qtdSentidosDespertados = 7;
             String constelacao = armadura.getConstelacao();
-            if ( !constelacao.equals("Áries") && !constelacao.equals("Touro") && !constelacao.equals("Escorpião") 
-            && !constelacao.equals("Virgem")
-            && !constelacao.equals("Peixes")
+            if ( !constelacao.equals("Áries") 
+            && !constelacao.equals("Touro")
+            && !constelacao.equals("Gêmeos")
             && !constelacao.equals("Câncer")
-            ) {
+            && !constelacao.equals("Virgem")
+            && !constelacao.equals("Leão")
+            && !constelacao.equals("Libra")
+            && !constelacao.equals("Escorpião")
+            && !constelacao.equals("Sagitário")
+            && !constelacao.equals("Capricórnio")
+            && !constelacao.equals("Aquário")
+            && !constelacao.equals("Peixes")) {
                 // dar erro
                 throw new Exception("Constelação inválida");
             }
@@ -73,6 +80,4 @@ public class Saint {
     }
 
 }
-
-
 
