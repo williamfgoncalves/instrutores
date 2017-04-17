560cb6e5dfa790812654fcc674a41abb9b0a8c9c
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2017-04-17 17:16:43 -0300

    feat(Silver, Gold): adicionar sub-classes para saint que representam os cavaleiros / amazonas de prata e ouro.

---
+++ modulo-01-cdz/SilverSaint.java
@@ -0,0 +1,8 @@
+// C#, C++
+// public class SilverSaint : Saint
+public class SilverSaint extends Saint {
+    public SilverSaint(String nome, Armadura armadura) throws Exception {
+        super(nome, armadura);
+        this.qtdSentidosDespertados = 6;
+    }
+}
