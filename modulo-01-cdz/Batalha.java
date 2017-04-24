e133c7d0b2191d261faa1420bc25e35d6eee4319
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2017-04-24 17:03:40 -0300

    feat(Batalha): implementar lógica de batalha por turnos.

--- modulo-01-cdz/Batalha.java
+++ modulo-01-cdz/Batalha.java
@@ -11,11 +11,26 @@ public class Batalha {
         int valor1 = this.saint1.getArmadura().getCategoria().getValor();
         int valor2 = this.saint2.getArmadura().getCategoria().getValor();
         final double dano = 10;
+        Saint saintEmAcao = null;
 
         if (valor1 >= valor2) {
+            saintEmAcao = this.saint1;
             this.saint2.perderVida(dano);
         } else {
+            saintEmAcao = this.saint2;
             this.saint1.perderVida(dano);
+        }
+
+        boolean nenhumMorto = true;
+        while (nenhumMorto) {
+            // 1. definindo quem vai atuar no round
+            saintEmAcao = saintEmAcao == this.saint1 ? this.saint2 : this.saint1;
+            // 2. executando próximo movimento
+            Movimento proximoMovimento = saintEmAcao.getProximoMovimento();
+            proximoMovimento.executar();
+            // 3. verificando se a batalha acabou
+            nenhumMorto = this.saint1.getStatus() != Status.MORTO &&
+            this.saint2.getStatus() != Status.MORTO;
         }
 
     }
