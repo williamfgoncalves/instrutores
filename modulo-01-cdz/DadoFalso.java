10b1af7cb0c072fe8385bee176bfd4ebc50104f5
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2017-04-26 16:56:54 -0300

    feat(DadoFalso): implementar classe fake para facilitar testes.

---
+++ modulo-01-cdz/DadoFalso.java
@@ -0,0 +1,11 @@
+public class DadoFalso implements Sorteador {
+    private int valor;
+    
+    public DadoFalso(int valor) {
+        this.valor = valor;
+    }
+    
+    public int sortear() {
+        return this.valor;
+    }
+}
