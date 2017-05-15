6e95ab46b79adeb46796f28c4f88d251fa1051d8
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2017-05-15 14:36:14 -0300

    feat(Séries): adicionar consulta de séries inválidas.

---
+++ modulo-04-javascript/dia02/js/exercicios-arrays.js
@@ -0,0 +1,17 @@
+console.log("series", series);
+
+// Ex 01
+// 1. criar funcao para verificar se série é inválida
+// 2. aplicar criterio de verificação de série inválida para as séries originais
+// 3. retornar string resultante de acordo com o formato esperado
+function seriesInvalidas(series) {
+  let invalidas = series.filter(serie => {
+    // for (let campo in serie) { }
+    let algumCampoInvalido = Object.values(serie).some(v => v === null || typeof v === 'undefined');
+    let estreiaInvalida = serie.anoEstreia > new Date().getFullYear();
+    return estreiaInvalida || algumCampoInvalido;
+  });
+  return `Séries Inválidas: ${ invalidas.map(s => s.titulo).join(" - ") }`;
+}
+
+//
