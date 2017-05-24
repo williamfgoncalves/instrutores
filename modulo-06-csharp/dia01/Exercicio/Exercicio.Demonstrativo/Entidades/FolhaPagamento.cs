636b3ebd67e0d510395dabec5033e8ce58ac0f38
Giovani Decusati <giovani.decusati@hotmail.com>
2017-05-24 20:45:36 -0300

    Estrutura inicial do exercicio demonstrativo de folha de pagamento

---
+++ modulo-06-csharp/dia01/Exercicio/Exercicio.Demonstrativo/Entidades/FolhaPagamento.cs
@@ -0,0 +1,15 @@
+﻿using Crescer.Exercicio3;
+using System;
+
+namespace Exercicio.Demonstrativo.Entidades
+{
+    public class FolhaPagamento : IFolhaPagamento
+    {
+
+        public Demonstrativo GerarDemonstrativo(int horasCategoria, double salarioBase, double horasExtras, double horasDescontadas)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ modulo-06-csharp/dia01/Exercicio/Exercicio.Demonstrativo/Entidades/HorasCalculadas.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio.Demonstrativo.Entidades
+{
+    public class HorasCalculadas
+    {
+        public HorasCalculadas(double qtdHoras, double valorTotalHoras)
+        {
+            QtdHoras = qtdHoras;
+            ValorTotalHoras = valorTotalHoras;
+        }
+        public double QtdHoras { get; private set; }
+        public double ValorTotalHoras { get; private set; }
+    }
+}
