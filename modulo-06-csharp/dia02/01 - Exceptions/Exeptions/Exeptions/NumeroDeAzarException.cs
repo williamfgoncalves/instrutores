7b91aee438230b5005c62eb07170040c726a7582
Fabricio Elias Rissetto da Silva <fabriciorissetto@gmail.com>
2017-05-25 15:00:21 -0300

    feat(exemplos): Exemplos vistos em aula (Exceptions,IO)

---
+++ modulo-06-csharp/dia02/01 - Exceptions/Exeptions/Exeptions/NumeroDeAzarException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exeptions
+{
+    public class NumeroDeAzarException : Exception
+    {
+        public NumeroDeAzarException() : base("Número 13 é um número de azar")
+        {
+        }
+    }
+
+
+}
---
+++ modulo-06-csharp/dia02/01 - Exceptions/Exeptions/Exeptions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exeptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numeroDigitado = Console.ReadLine();
+            try
+            {
+                var numeroConvertido = Convert.ToInt32(numeroDigitado);
+
+                if (numeroConvertido == 13)
+                    throw new NumeroDeAzarException();
+
+                Console.WriteLine("Digitou: " + numeroConvertido);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Somente números são permitidos.");
+            }
+            catch (NumeroDeAzarException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch
+            {
+                Console.WriteLine("Ocorreu um erro...");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
