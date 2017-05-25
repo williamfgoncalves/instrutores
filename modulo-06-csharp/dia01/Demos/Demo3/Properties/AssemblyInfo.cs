f8115bf5d3c3608d0f9939a489df11a6917c542b
Giovani Decusati <giovani.decusati@hotmail.com>
2017-05-24 21:05:01 -0300

    feat(demonstrações): demonstrações realizadas em aula

---
+++ modulo-06-csharp/dia01/Demos/Demo3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Demo3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Demo3")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("717723cc-8cf3-4aae-b6bb-15812703255f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ modulo-06-csharp/dia01/Demos/Exercicio1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        // função estática não está associada a uma instância de uma classe
+        static void Main(string[] args)
+        {
+            int[] entradas = new int[] { };
+            // int[] entradas = new int[3] {10,11,22 };
+            // var entradas = new int[] { };
+            // var entradas = new int[5] { 0, 1, 2, 3, 4 };
+            bool continua = true;
+
+            Console.WriteLine("#---------DIGITE SOMENTE NÚMEROS (\"exit\" pra sair)---------#");
+
+            while (continua)
+            {
+                var linhaLida = Console.ReadLine();
+                if (linhaLida == "exit")
+                {
+                    break;
+                }
+
+                var nrEntradas = entradas.Length;
+
+                var entradasAux = new int[nrEntradas + 1];
+
+                for (int i = 0; i < nrEntradas; i++)
+                {
+                    entradasAux[i] = entradas[i];
+                }
+
+                entradasAux[nrEntradas] = int.Parse(linhaLida);
+
+                entradas = entradasAux;
+            }
+
+            foreach (var entrada in entradas)
+            {
+                Console.WriteLine(entrada);
+            }
+
+            Console.WriteLine("#---------FIM---------#");
+            Console.ReadKey();
+        }
+
+    }
+}
