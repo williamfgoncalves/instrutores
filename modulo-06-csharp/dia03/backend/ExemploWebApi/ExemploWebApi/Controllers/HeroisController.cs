1268a9ea973a0519d18e242ca800cc5c93aa10f2
fabriciosilva <fabriciorissetto@gmail.com>
2017-05-26 18:08:38 -0300

    feat(exemplos): Exemplos vistos em aula (AngularJS + ASP.NET Web API)

---
+++ modulo-06-csharp/dia03/backend/ExemploWebApi/ExemploWebApi/Controllers/HeroisController.cs
@@ -0,0 +1,55 @@
+﻿using ExemploWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExemploWebApi.Controllers
+{
+    public class HeroisController : ApiController
+    {
+        private static List<Heroi> herois = new List<Heroi>();
+        private static int contador = 1;
+        private static object @lock = new object();
+
+        public IEnumerable<Heroi> Get(string nome = null, int? id = null)
+        {
+            return herois.Where(x => 
+                (id == null || x.Id == id) &&
+                (nome == null || x.Nome == nome)
+            );
+        }
+
+        public IHttpActionResult Post(Heroi heroi)
+        {
+            if (heroi == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                lock (@lock)
+                {
+                    herois.Add(heroi);
+                    heroi.Id = contador++;
+                }
+
+                return Ok(heroi);
+            }
+        }
+
+        //public HttpResponseMessage Post(Heroi heroi)
+        //{
+        //    if (heroi != null)
+        //    {
+        //        return new HttpResponseMessage(HttpStatusCode.Created);
+        //    }
+        //    else
+        //    {
+        //        return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        //    }
+        //}
+    }
+}
