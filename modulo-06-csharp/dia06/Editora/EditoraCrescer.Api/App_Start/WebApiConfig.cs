9b915a6c20c4ae26fef3ef436c0ac0b43f808399
fabriciosilva <fabriciorissetto@gmail.com>
2017-06-02 16:53:36 -0300

    feat(cors): habilitando cors

--- modulo-06-csharp/dia06/Editora/EditoraCrescer.Api/App_Start/WebApiConfig.cs
+++ modulo-06-csharp/dia06/Editora/EditoraCrescer.Api/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace EditoraCrescer.Api
 {
@@ -10,6 +11,7 @@ namespace EditoraCrescer.Api
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
