2071ec965d40caad4f87ed8bcffc20cfe8dc60db
carloshenrique <carloshenrique@DESKTOP-8DE3FPB>
2017-06-23 16:49:28 -0300

    feat(aula4): JPA examplo

---
+++ modulo-08-java/aula4/src/main/java/br/com/crescer/aula4/Run.java
@@ -0,0 +1,32 @@
+package br.com.crescer.aula4;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * @author carloshenrique
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("CRESCER");
+        final EntityManager em = emf.createEntityManager();
+
+        
+        final Cliente cliente = em.find(Cliente.class, 1l);
+        cliente.setNome("Pedro Henrique Nonnemacher");
+        em.detach(cliente);
+        
+        em.getTransaction().begin();
+        em.merge(cliente);
+        em.getTransaction().commit();
+
+        System.out.println(cliente.getNome());
+
+//        em.persist(cliente);
+        em.close();
+        emf.close();
+
+    }
+}
