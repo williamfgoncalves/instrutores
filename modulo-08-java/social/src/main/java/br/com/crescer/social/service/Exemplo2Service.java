5a277282b92ffe047636c4490d014bc8b84f9238
Carlos H. Nonnemacher <carloshenrique@cwi.com.br>
2017-07-07 09:32:35 -0300

    Ex. de mock para teste e exemplo de APO

---
+++ modulo-08-java/social/src/main/java/br/com/crescer/social/service/Exemplo2Service.java
@@ -0,0 +1,18 @@
+package br.com.crescer.social.service;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author carloshenrique
+ */
+@Service
+public class Exemplo2Service {
+
+    public BigDecimal getTen() {
+        return BigDecimal.TEN;
+    }
+
+    
+}
---
+++ modulo-08-java/social/src/main/java/br/com/crescer/social/service/ExemploService.java
@@ -0,0 +1,21 @@
+package br.com.crescer.social.service;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author carloshenrique
+ */
+@Service
+public class ExemploService {
+
+    @Autowired
+    private Exemplo2Service service2;
+
+    public BigDecimal get() {
+        return service2.getTen();
+    }
+
+}
